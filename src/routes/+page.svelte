39e7195dc56983dc85ae7a54adb3b664f7ae38dd
Eric Chen <ericchen314@gmail.com>
2023-07-19 13:38:59 -0700

    Initialize root page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class=" text-green-500">Eat My Ass</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-accent">Button</button>
