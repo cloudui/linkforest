8c282f347eb3ec4ab1dc6450c715104c95498e1f
Eric Chen <ericchen314@gmail.com>
2023-07-22 00:48:08 -0700

    Fix login page typos and remove sign out button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -29,8 +29,8 @@
 {#if $user}
     <h2 class="card-title">Welcome, {$user.displayName}</h2>
     <p class="text-center text-success">You are logged in</p>
-    <button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
-    <a href="/login/username" class="btn btn-primary mt-3">Upload Profile Image</a>
+    <!-- <button class="btn btn-warning" on:click={signOutSSR}>Sign out</button> -->
+    <a href="/login/username" class="btn btn-primary mt-3">Choose Username</a>
 {:else}
     <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
