8aa23561ec80d6373e4faf6b28b743863a772477
Eric Chen <ericchen314@gmail.com>
2023-07-22 00:44:24 -0700

    Create general error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { page } from '$app/stores';
+    import Navbar from '$lib/components/Navbar.svelte';
+</script>
+  
+<Navbar username="/" isUser={false}/>
+<div class="w-full pt-12 mx-auto">
+    <main class="prose text-center mx-auto">
+        <h1 class="text-7xl text-error font-bold mb-3">
+            {$page.status} Error
+        </h1>
+        <p class="text-xl font-bold mb-8">{$page.error?.message || "Willy Wonka!"}</p>
+
+        <img class="mx-auto my-5 rounded-lg" alt="error" src="/error.webp" />
+    </main>
+</div>
