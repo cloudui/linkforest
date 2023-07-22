fe971f29cc093cd180fab3e0f1de09be32a62e7b
Eric Chen <ericchen314@gmail.com>
2023-07-22 00:41:54 -0700

    Add bio editing and profile publicity toggle to edit route

--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -1,4 +1,5 @@
   <script lang="ts">
+    import type { PageData } from "./$types";
     import { page } from "$app/stores";
     import UserLink from "$lib/components/UserLink.svelte";
     import { db, userData, user } from "$lib/firebase";
@@ -11,13 +12,19 @@
     } from "firebase/firestore";
     import { writable } from "svelte/store";
     import SortableList from "$lib/components/SortableList.svelte";
+    // import { get } from "svelte/store";
+    // import { enhance } from "$app/forms";
+
+    export let data: PageData;
   
     const icons = [
+      "Blog",
       "Twitter",
       "YouTube",
       "TikTok",
       "LinkedIn",
       "GitHub",
+      "Instagram",
       "Custom",
     ];
 
@@ -30,11 +37,13 @@
     const formData = writable(formDefaults);
   
     let showForm = false;
+    let editBio = false;
+    let bio = data.bio || "";
   
     $: urlIsValid = $formData.url.match(/^(ftp|http|https):\/\/[^ "]+$/);
     $: titleIsValid = $formData.title.length < 20 && $formData.title.length > 0;
     $: formIsValid = urlIsValid && titleIsValid;
-
+    $: bioIsValid = bio.length <= 260;
   
     async function addLink(e: SubmitEvent) {
       const userRef = doc(db, "users", $user!.uid);
@@ -54,7 +63,6 @@
   
       showForm = false;
     }
-
   
     async function deleteLink(item: any) {
       const userRef = doc(db, "users", $user!.uid);
@@ -74,14 +82,99 @@
       setDoc(userRef, { links: newList }, { merge: true });
     }
   
+    async function toggleProfileStatus(item: any) {
+      const userRef = doc(db, "users", $user!.uid);
+      await updateDoc(userRef, {
+        published: !$userData?.published,
+      });
+    }
+
+    function toggleBio() {
+      editBio = true;
+    }
+
+    async function handleBioSubmit(e: SubmitEvent) {
+      if (!bioIsValid) return;
+
+      const userRef = doc(db, "users", $user!.uid);
+      await updateDoc(userRef, {
+        bio: bio,
+      });
+      editBio = false;
+    }
 </script>
  
 <div class="mb-12 pb-12">
   <main class="max-w-xl mx-auto">
     {#if $userData?.username == $page.params.username}
+    <div class="profile-head mb-3">
       <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
         Edit your Profile
       </h1>
+
+      <div class="text-center mb-6">
+          <p>
+            Profile Link:
+            <a
+              href={`/${$userData?.username}`}
+              class="link link-accent"
+            >
+              https://linkforest.echen.io/{$userData?.username}
+            </a>
+          </p>
+        </div>
+
+        <div class="text-center my-4">
+          <a class="btn btn-outline btn-xs" href="/login/photo">Change photo</a>
+          <!-- <a class="btn btn-outline btn-xs" href={`/${$userData.username}/bio`}>Edit Bio</a> -->
+        </div>
+
+        <form class="form-control">
+          <label class="label cursor-pointer flex items-start justify-center">
+            <span class="label-text mr-6">
+              <div
+                class="tooltip group-hover:tooltip-open"
+                data-tip="If public, the world can see your profile"
+              >
+                {$userData?.published ? "Public profile" : "Private profile"}
+              </div>
+            </span>
+            <input
+              type="checkbox"
+              class="toggle toggle-success"
+              checked={$userData?.published}
+              on:change={toggleProfileStatus}
+            />
+          </label>
+        </form>
+      </div>
+
+      <div class="bio mb-5 text-center">
+        <p class="text-success mr-1 mb-3">Current Bio:</p>
+        {#if editBio }
+          <form on:submit|preventDefault={handleBioSubmit}>
+            <div class="form-control">
+              <textarea
+                name="bio"
+                class="textarea textarea-bordered border-blue-300 leading-normal py-2 input-error"
+                class:textarea-error={!bioIsValid}
+                class:border-error={!bioIsValid}
+                rows="1"
+                bind:value={bio}
+              />
+            </div>
+            {#if bioIsValid}
+              <button class="btn btn-outline btn-success btn-sm my-3">Update Bio</button>
+            {:else}
+              <p class="text-error text-right text-xs my-3">Bio must be less than 260 characters.</p>
+            {/if}
+          </form>
+        {:else}
+          <span class="border-solid border-2 border-blue-400 p-2 rounded-md">
+            <button type="submit" on:click={toggleBio}>{!bio ? "<empty bio>" : bio}</button> 
+          </span>
+        {/if}
+      </div>
 
       <!-- INSERT sortable list here -->
       <SortableList list={$userData?.links} on:sort={sortList} let:item let:index>
