4402d87fca73545d4836d3dfb3cb8d6cfbca376b
Eric Chen <ericchen314@gmail.com>
2023-07-22 00:45:38 -0700

    Add SEO meta using $page.data to base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
+    import { user, userData } from "$lib/firebase";
+    import { page } from "$app/stores";
     import "../app.css";
-</script>
+    // import type { LayoutData } from "./$types";
+
+    // export let data: LayoutData; 
+     
+    $user;
+    $userData; 
+
+    let title = $page.data.title;
+    let description = $page.data.description;
+    let imageUrl = $page.data.imageUrl;
+    let twitterCard = $page.data.twitterCard;
+
+ </script>
+ 
+ <svelte:head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    
+    <!-- OpenGraph (OG) metadata --> 
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={imageUrl} />
+    <meta property="og:url" content={$page.url.pathname} />
+    
+    <!-- Twitter metadata --> 
+    <meta name="twitter:card" content={twitterCard} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={imageUrl} />
+</svelte:head>
 
 <div class="min-h-screen flex flex-col">
+    
     <slot />
 </div>
